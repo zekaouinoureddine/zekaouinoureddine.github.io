0416011e2aad6a54bd553b468d118fc3381f4e58
Nour Eddine ZEKAOUI <61702091+zekaouinoureddine@users.noreply.github.com>
2021-01-27 05:27:49 +0100

    Dark Mode Integration 🌓

--- js/scripts.js
+++ js/scripts.js
@@ -64,6 +64,25 @@
         $(this).removeClass("floating-label-form-group-with-focus");
       });
     });
+
+    // Dark Mode
+  const options = {
+    bottom: '32px', // default: '32px'
+    right: '32px', // default: '32px'
+    left: 'unset', // default: 'unset'
+    time: '0.5s', // default: '0.3s'
+    mixColor: '#fff', // default: '#fff'
+    backgroundColor: '#fff',  // default: '#fff'
+    buttonColorDark: '#100f2c',  // default: '#100f2c'
+    buttonColorLight: '#fff', // default: '#fff'
+    saveInCookies: false, // default: true,
+    label: '🌓', // default: ''
+    autoMatchOsTheme: true // default: true
+  }
+  
+  const darkmode = new Darkmode(options);
+  darkmode.showWidget();
   
   })(jQuery); // End of use strict
+  
   
