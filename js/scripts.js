49bbfbefae834147ed453051938d37517289eda4
Nour Eddine ZEKAOUI <61702091+zekaouinoureddine@users.noreply.github.com>
2021-01-27 05:31:18 +0100

    🌓 Dark Mode Integration

--- js/scripts.js
+++ js/scripts.js
@@ -68,8 +68,8 @@
     // Dark Mode
   const options = {
     bottom: '32px', // default: '32px'
-    right: '32px', // default: '32px'
-    left: 'unset', // default: 'unset'
+    right: 'unset', // default: '32px'
+    left: '32px', // default: 'unset'
     time: '0.5s', // default: '0.3s'
     mixColor: '#fff', // default: '#fff'
     backgroundColor: '#fff',  // default: '#fff'
